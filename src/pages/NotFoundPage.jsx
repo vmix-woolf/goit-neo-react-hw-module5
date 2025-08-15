6485ba208f5a1be1ba07f1b2a5f5fe4bc2ae7146
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2025-08-15 21:56:04 +0300

    add not found page and its css module

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom'
+import styles from './NotFoundPage.module.css'
+
 export default function NotFoundPage() {
     return (
-        <div>
-            <h1>404 - Not Found</h1>
-            <Link to="/">Go Home</Link>
-        </div>
+        <main className={styles.wrapper}>
+            <h1 className={styles.title}>404 — Page Not Found</h1>
+            <p className={styles.text}>
+                На жаль, така сторінка не існує. Повернутися на головну?
+            </p>
+            <Link to="/" className={styles.link}>Go Home</Link>
+        </main>
     )
 }
+
