b00df0018e3c223957c22c26d39b4d7e964efcb6
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2025-08-15 21:08:32 +0300

    add step-03 and step-04 movieList and create posters

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,35 @@
+import { Link, useLocation } from 'react-router-dom'
+import styles from './MovieList.module.css'
+
+const defaultImg =
+    'https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg'
+
+export default function MovieList({ movies }) {
+    const location = useLocation()
+
+    return (
+        <ul className={styles.list}>
+            {movies.map(movie => (
+                <li key={movie.id} className={styles.item}>
+                    <Link
+                        to={`/movies/${movie.id}`}
+                        state={{ from: location }}
+                        className={styles.link}
+                    >
+                        <img
+                            src={
+                                movie.poster_path
+                                    ? `https://image.tmdb.org/t/p/w200/${movie.poster_path}`
+                                    : defaultImg
+                            }
+                            alt={movie.title || movie.name}
+                            className={styles.poster}
+                        />
+                        <p className={styles.title}>{movie.title || movie.name}</p>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
